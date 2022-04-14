b0c0fa8d56e2c6a4e5c95e22eee36bb25c3c3778
Leonid Krainov <krajnovleonid@gmail.com>
2022-04-14 23:07:36 +0300

    Create project

---
+++ src/app/MovieCard/moviecard.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-moviecard',
+  templateUrl: './moviecard.component.html',
+  styleUrls: ['/moviecard.component.scss']
+})
+
+export class MoviecardComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MoviecardComponent} from './MovieCard/moviecard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviecardComponent
   ],
   imports: [
     BrowserModule,
